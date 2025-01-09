4190ecd57f66535a8b80252316b0949c31e93c26
sandy0v0 <be0324ss@gmail.com>
2025-01-09 14:34:01 +0800

    建立院線片清單畫面架構

--- back/movie.php
+++ back/movie.php
@@ -1,1 +1,30 @@
-movie
+<button>新增電影</button>
+<hr>
+<div style="height:425px; overflow:auto;">
+<div style="display:flex;">
+    <div style="width:10%;">
+        <img src="" alt="">
+    </div>
+    <div style="width:10%;">
+        分級:
+    </div>
+    <div style="width:80%;">
+        <div style="display:flex;text-align:center;justify-content:space-between">
+            <div>片名：</div>
+            <div>片長：</div>
+            <div>上映時間：</div>
+        </div>
+        <div>
+            <button>隱藏</button>
+            <button>往上</button>
+            <button>往下</button>
+            <button>編輯電影</button>
+            <button>刪除電影</button>
+        </div>
+        <div>
+            劇情介紹：
+        </div>
+    </div>
+</div>
+<hr>
+</div>

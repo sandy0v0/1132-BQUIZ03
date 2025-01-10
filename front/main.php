69d960a1e2187cbfa90d4857558c3cd46534c566
sandy0v0 <be0324ss@gmail.com>
2025-01-10 16:33:45 +0800

    完成預告片自動輪播轉場

--- front/main.php
+++ front/main.php
@@ -83,7 +83,7 @@
                      $posters=$Poster->all(['sh'=>1]," order by rank");
                      foreach($posters as $idx => $poster):
                 ?>
-                <div class="poster">
+                <div class="poster" data-ani="<?=$poster['ani'];?>">
                     <!-- div.poster -->
                     <img src="./upload/<?=$poster['img'];?>" alt="">
                     <span><?=$poster['name'];?></span>
@@ -113,6 +113,39 @@
 <script>
     // 將圖片抓出來,並show第一張圖片
     $(".poster").eq(0).show();
+
+    let slider=setInterval(() => {
+        sliders();
+    }, 2500);
+
+    function sliders(){
+        let now=$(".poster:visible").index();
+        let next=($(".poster").length==now+1)?0:now+1;
+        let ani=$(".poster").eq(next).data('ani');
+        //console.log(now,next)
+
+        switch(ani){
+            case 1:
+                //淡入淡出
+                $(".poster").eq(now).fadeOut(1000,function(){
+                    $(".poster").eq(next).fadeIn(1000);
+                });
+            break;
+            case 2:
+                //縮放
+                $(".poster").eq(now).hide(1000,function(){
+                    $(".poster").eq(next).show(1000);
+                });
+            break;
+            case 3:
+            //滑入滑出
+                $(".poster").eq(now).slideUp(1000,function(){
+                    $(".poster").eq(next).slideDown(1000);
+                });
+
+            break;
+        }
+    }
 
 </script>
 
